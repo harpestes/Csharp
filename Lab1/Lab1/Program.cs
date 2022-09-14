54b24ae1e111dd5d8ec3bf697881d2641f9c7a19
harpestes <dikaloandrij@gmail.com>
2022-09-14 15:51:17 +0300

    was added lab1/2

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double t, V, g = 9.81, h;
+            bool isOkV = true, isOkH = true;
+            do
+            {
+                Console.WriteLine("Введiть значення V: ");
+                string strV = Console.ReadLine();
+                Console.WriteLine("Введiть значення H: ");
+                string strH = Console.ReadLine();
+
+                V = double.Parse(strV);
+                h = double.Parse(strH);
+
+                isOkV = double.TryParse(strV, out V);
+                isOkH = double.TryParse(strH, out h);
+            }
+            while (!isOkV && !isOkH);
+
+            t = (Math.Sqrt(2 * g * h) - V) / g;
+            Console.WriteLine("Час: " + Math.Round(t, 2, MidpointRounding.AwayFromZero) + " с");
+        }
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn, nk; 
+            double result = 0;
+            bool isNNOk, isNKOk;
+            do
+            {
+                Console.WriteLine("NN: ");
+                string strNN = Console.ReadLine();
+                isNNOk = int.TryParse(strNN, out nn);
+                if (!isNNOk && nn < 0)
+                {
+                    Console.WriteLine("Введiть вiрне nn");
+                }
+            }
+            while (!isNNOk);
+            do
+            {
+                Console.WriteLine("NK: ");
+                string strNK = Console.ReadLine();
+                isNKOk = int.TryParse(strNK, out nk);
+                if (!isNKOk && nk < 0)
+                {
+                    Console.WriteLine("Введiть вiрне nk");
+                }
+            }
+            while (!isNNOk);
+            for(int k = nn; k < nk; k++)
+            {
+                result += (Math.Pow(-1, (Math.Pow(k, 2) + k + 1)) * Math.Pow(k, 2)) / (2 * Math.Pow(k, 2) + 5);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
